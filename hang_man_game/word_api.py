3560014b70e6bfe3c518a867943a3eebd53d910f
Raphael Torres <110772451+raphavtorres@users.noreply.github.com>
2023-03-17 16:56:56 -0300

    Update word_api.py

--- hang_man_game/word_api.py
+++ hang_man_game/word_api.py
@@ -1,14 +1,18 @@
 import requests
 
+# PROXIES = {
+#     ...
+# }
+
 
 def get_api_random():
     while True:
-        word_url = "https://random-word-api.herokuapp.com/word?number=1"
-        word_response = requests.get(word_url)
+        word_url = "http://random-word-api.herokuapp.com/word?number=1"
+        word_response = requests.get(word_url) #, proxies=PROXIES)
         word = word_response.json()[0]
 
-        definition_url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
-        definition_response = requests.get(definition_url)
+        definition_url = f"http://api.dictionaryapi.dev/api/v2/entries/en/{word}"
+        definition_response = requests.get(definition_url) #, proxies=PROXIES)
 
         try:
             definition = definition_response.json()[0]['meanings'][0]['definitions'][0]['definition']
